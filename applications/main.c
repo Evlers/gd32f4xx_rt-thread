5b38d28ce9805185e220ad09cae0dbf960741f2a
Evlers <1425295900@qq.com>
2024-01-25 20:24:33 +0800

    Add wifi profile to support automatic connection

--- applications/main.c
+++ applications/main.c
@@ -22,48 +22,22 @@
 #define DBG_LVL             DBG_INFO
 #include "rtdbg.h"
 
-/* defined the LED1 pin: PC6 */
-#define LED1_PIN GET_PIN(C, 6)
+/* defined the LED_R pin: PE7 */
+#define LED1_PIN GET_PIN(E, 7)
 
 int main(void)
 {
-    int count = 1;
-
-    /* Initializes the flash abstraction layer */
-    fal_init();
-
-    /* Create a block device using the flash abstraction layer */
-    fal_blk_device_create("root");
-
-    /* Mount the FAT file system to the root directory */
-    if (dfs_mount("root", "/", "elm", 0, 0) != 0)
-    {
-        if (!dfs_mkfs("elm", "root")) 
-        {
-            if (dfs_mount("root", "/", "elm", 0, 0) != 0)
-            {
-                LOG_E("The fat file system failed to be mounted!");
-            }
-        }
-        else
-        {
-            LOG_E("FAT file system formatting failed!");
-        }
-    }
-
-    /* Initializes the easyflash */
-    easyflash_init();
+    /* enable auto reconnect on WLAN device */
+    rt_wlan_config_autoreconnect(RT_TRUE);
 
     /* set LED1 pin mode to output */
     rt_pin_mode(LED1_PIN, PIN_MODE_OUTPUT);
 
-    while (count++)
+    while (1)
     {
         rt_pin_write(LED1_PIN, PIN_HIGH);
         rt_thread_mdelay(500);
         rt_pin_write(LED1_PIN, PIN_LOW);
         rt_thread_mdelay(500);
     }
-
-    return RT_EOK;
 }
