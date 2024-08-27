42b6ac2915a1eef07ab02ea91e8163a02db4e926
Evlers <1425295900@qq.com>
2024-08-27 15:29:39 +0800

    update chip resource configure

--- board/board.h
+++ board/board.h
@@ -1,13 +1,13 @@
 /*
- * Copyright (c) 2006-2023, RT-Thread Development Team
+ * Copyright (c) 2006-2024 RT-Thread Development Team
  *
  * SPDX-License-Identifier: Apache-2.0
  *
  * Change Logs:
  * Date           Author       Notes
- * 2021-08-20     BruceOu      first implementation
- * 2024-01-25     Evlers       For GD32F427VI chip
+ * 2024-08-27     Evlers       first implementation
  */
+
 #ifndef __BOARD_H__
 #define __BOARD_H__
 
@@ -24,17 +24,17 @@
 #define GD32_FLASH_SIZE                 (2048 * 1024)
 #define GD32_FLASH_PAGE_SIZE            (4 * 1024)
 #define GD32_FLASH_END_ADDRESS          ((uint32_t)(GD32_FLASH_START_ADRESS + GD32_FLASH_SIZE))
+#define EXT_SDRAM_BEGIN                 (0xC0000000U) /* the begining address of external SDRAM */
+#define EXT_SDRAM_END                   (EXT_SDRAM_BEGIN + (32U * 1024 * 1024)) /* the end address of external SDRAM */
 
-// <o> Internal SRAM memory size[Kbytes] <8-512>
-//  <i>Default: 704
-#ifdef __ICCARM__
-// Use *.icf ram symbal, to avoid hardcode.
-extern char __ICFEDIT_region_RAM_end__;
-#define GD32_SRAM_END                   &__ICFEDIT_region_RAM_end__
-#else
-#define GD32_SRAM_SIZE                  704
+/* SRAM Size:704KB, Addr:0x20000000  TCM RAM Size: 64KB, Addr:0x10000000 */
+#define GD32_SRAM0_SIZE                 112
+#define GD32_SRAM1_SIZE                 16
+#define GD32_SRAM2_SIZE                 64
+#define GD32_ADDSRAM_SIZE               512
+#define GD32_TCMRAM_SIZE                64  // TCMSRAM(紧耦合存储器SRAM) 只可被Cortex®-M4内核的数据总线访问
+#define GD32_SRAM_SIZE                  (GD32_SRAM0_SIZE + GD32_SRAM1_SIZE + GD32_SRAM2_SIZE + GD32_ADDSRAM_SIZE)
 #define GD32_SRAM_END                   (0x20000000 + GD32_SRAM_SIZE * 1024)
-#endif
 
 #ifdef __ARMCC_VERSION
 extern int Image$$RW_IRAM1$$ZI$$Limit;
@@ -50,4 +50,3 @@ extern int __bss_end;
 #define HEAP_END                        GD32_SRAM_END
 
 #endif
-
